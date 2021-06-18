6c1dc1ffad64ce94d29dbcd1bff018ad68e9144e
Olivia Park <oliviaypark113@gmail.com>
2021-06-17 22:17:05 -0400

    updated Intro.jsx

--- src/components/intro/Intro.jsx
+++ src/components/intro/Intro.jsx
@@ -1,9 +1,38 @@
-import React from 'react'
+import {useEffect, useRef } from 'react'
 import './intro.scss'
+import {init} from 'ityped'
 
-export default function intro() {
+
+export default function Intro() {
+
+  const textRef = useRef()
+
+  useEffect(()=>{
+  
+    init(textRef.current, { showCursor: false, 
+      backDelay:1500,
+      strings: 
+      ['Designer', 'Developer','Content Creator' ] })
+    
+
+  },[])
   return (
     <div className="intro" id="intro">
+      <div className="left">
+        <div className="imgContainer">
+          <img src="assets/olivia_photo.png" alt="olivia_photo"/>
+        </div>
+      </div>
+      <div className="right">
+        <div className="wrapper">
+          <h2>Hi There, I'm</h2>
+          <h1>Olivia Park</h1>
+          <h3>Full Stack <span ref={textRef}></span>  </h3>
+        </div>
+        <a href="#portfolio">
+          <img src="assets/down.png" alt=""/>
+        </a>
+      </div>
     
     </div>
   )
